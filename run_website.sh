f6f6cf4ac79fb2676b09051bfd67eb2ec865c337
Lett <BnDLett@users.noreply.github.com>
2025-05-02 20:43:22 -0400

    Switch to 8080 for bind port

--- run_website.sh
+++ run_website.sh
@@ -7,9 +7,9 @@ echo "Starting auto-updater job."
 echo "Starting gunicorn."
 if [ -e server.crt ] && [ -e server.key ]
 then
-  gunicorn -w 4 'main:app' -b 0.0.0.0 --certfile=server.crt --keyfile=server.key
+  gunicorn -w 4 'main:app' -b 0.0.0.0:8080 --certfile=server.crt --keyfile=server.key
 else
-  gunicorn -w 4 'main:app' -b 0.0.0.0
+  gunicorn -w 4 'main:app' -b 0.0.0.0:8080
 fi
 
 kill "$(jobs -p)"
