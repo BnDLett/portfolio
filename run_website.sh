c69a7843c5a476e14d98b257670266c4bbb300a9
Lett <BnDLett@users.noreply.github.com>
2025-05-02 21:13:37 -0400

    Discord embed and hot reloading

--- run_website.sh
+++ run_website.sh
@@ -7,9 +7,9 @@ echo "Starting auto-updater job."
 echo "Starting gunicorn."
 if [ -e server.crt ] && [ -e server.key ]
 then
-  gunicorn -w 4 'main:app' -b 0.0.0.0:8080 --certfile=server.crt --keyfile=server.key
+  gunicorn -w 4 'main:app' -b 0.0.0.0:8080 --certfile=server.crt --keyfile=server.key --reload
 else
-  gunicorn -w 4 'main:app' -b 0.0.0.0:8080
+  gunicorn -w 4 'main:app' -b 0.0.0.0:8080 --reload
 fi
 
 kill "$(jobs -p)"
